89303e15c9277000c09af5c580b19b4361aebc2f
Oliver Silvester <oliver.silvester@diamond.ac.uk>
2023-03-06 14:12:04 +0000

    added frame.py

---
+++ ptypy/experiment/frame.py
@@ -0,0 +1,29 @@
+"""Stores all information about a frame, to be used by pre-processing to encode and processing to decode"""
+
+import numpy as np
+class Frame():
+    
+    def __init__(self, posx, posy, data, dtype, shape):
+        self.posx = posx
+        self.posy = posy
+        self.data = data
+        self.dtype = dtype
+        self.shape = shape #Assume always square shape
+
+        
+    #should add checks to see if already encoded
+    def encode(self):
+        self.posx = str(self.posx).encode()
+        self.posy = str(self.posy).encode()
+        self.dtype = str(self.data.dtype).encode()
+        self.shape = str(self.data.shape).encode()
+          
+    #Not the inverse of encode() since data isn't encoded there, (numpy arrays already have buffer interface)
+    def decode(self):
+        self.posx = self.posx.decode()
+        self.posy = self.posy.decode()
+        self.dtype = self.dtype.decode()
+        self.shape = int(self.shape.decode()[1:].split(",")[0])
+        self.data = np.frombuffer(self.data,dtype=self.dtype)
+        self.data = self.data.reshape(self.shape,self.shape)
+        
